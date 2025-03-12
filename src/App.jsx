f8be2b7fd55aed215f436dce1f3461af7f2b08a1
Christian Wirsching <christianwirsching@gmail.com>
2025-03-12 17:58:02 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
+import listingData from './assets/listingData.json'
 import Footer from './components/Footer'
+import ListingItem from './components/ListingItem';
 import Navbar from './components/Navbar'
+import React,{useState} from "react";
+
 
 function App() {
+
+  const [listingDataState, setListingDataState] = useState(listingData);
+  
+  const handleDelete=(id)=>{
+    setListingDataState(listingDataState.filter((item)=>item.id!==id));
+  };
+
   return (
     <div className = "App">
     <Navbar/>
+    
+    {listingDataState.map((oneItem)=>{
+         return <ListingItem oneItem = {oneItem} key = {oneItem.id} handleDelete = {handleDelete}/>  
+    })  
+    }
+    
     <Footer/>
     </div>
   );
---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,27 @@
+
+
+const ListingItem = ({oneItem, handleDelete}) => {
+
+  return (
+    <div className = "listing-item">
+      <div className="listing-details">
+        <img src={oneItem.picture_url} alt={oneItem.name} className='listing-image'/>
+        <div className = "small-details">
+            {oneItem.host_is_superhost && <div className = "super-host-conditional"> ✨ Super Host</div>}
+            <p>Neighbourhood: {oneItem.neighbourhood}</p>
+            <p>Price:{oneItem.price}</p>
+            <p>Minimum Nights:{oneItem.minimum_nights}</p>
+            <p>Review Score:{oneItem.review_scores_ratings}</p>
+        </div>
+        </div>
+
+        <h3>{oneItem.name}</h3>
+        <p>{oneItem.description}</p>
+          
+          <button className="delete-button" onClick={()=>handleDelete(oneItem.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default ListingItem
+
